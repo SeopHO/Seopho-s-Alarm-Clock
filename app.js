44b3d0fd7060625882ba65a0d0a95a547984f4a3
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-01-20 00:01:25 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,96 @@
-const view = document.querySelectorAll(".view");
+const views = document.querySelectorAll(".view");
 
-const view_hour = document.querySelector(".view-hour");
-const view_minute = document.querySelector(".view-minute");
-const view_second = document.querySelector(".view-second");
+const view_hour = document.querySelector(".view-hour h1");
+const view_minute = document.querySelector(".view-minute h1");
+const view_second = document.querySelector(".view-second h1");
 
 const start_btn = document.querySelector(".start-btn");
 
 let time_pos = null;
 
+let hour_value=0;
+let min_value=0;
+let sec_value=0;
+
+const _HOUR = "hour";
+const _MIN = "minute";
+const _SEC = "second";
+
+views.forEach(function(view)
+{
+    view.addEventListener("click",function(event)
+    {
+        const style = event.currentTarget.classList;
+        console.log(style);
+        if(style.contains("view-hour"))
+        {
+            time_pos=_HOUR;
+        }
+        else if(style.contains("view-minute"))
+        {
+            time_pos = _MIN;
+        }
+        else if(style.contains("view-second"))
+        {
+            time_pos = _SEC;
+        }
+        console.log(time_pos);
+        
+    })
+});
+
+window.addEventListener("keydown",function(event)
+{
+    switch(event.keyCode)
+    {
+        case 38:
+            console.log('위');
+            increaseHandler();
+            break;
+        case 40:
+            console.log('아래');
+            decreaseHandler();
+            break;
+    }
+});
+
+function increaseHandler()
+{
+    switch(time_pos)
+    {
+        case _HOUR:
+            hour_value++;
+            view_hour.textContent = hour_value;
+            break;
+        case _MIN:
+            min_value++;
+            view_minute.textContent = min_value;
+            break;
+        case _SEC:
+            sec_value++;
+            view_second.textContent = sec_value;
+            break;
+    }
+    
+}
+
+function decreaseHandler()
+{
+    switch(time_pos)
+    {
+        case _HOUR:
+            hour_value--;
+            view_hour.textContent = hour_value;
+            break;
+        case _MIN:
+            min_value--;
+            view_minute.textContent = min_value;
+            break;
+        case _SEC:
+            sec_value--;
+            view_second.textContent = sec_value;
+            break;
+    }
+}
 
 
-// const styles_d = event.currentTarget.classList;
-// if(styles_d.contains("decrease"))
-// {
-//     if(styles_d.contains("hour-decrease"))
-//     {
-//         hour--;
-//         if(hour < 0)
-//             hour = 23;
-//         view_hour.textContent = detail(hour);
-//     }
-//     else if(styles_d.contains("minute-decrease"))
-//     {
-//         min--;
-//         min = detail2(min);
-//         view_minute.textContent = detail(min);
-//     }
-//     else if(styles_d.contains("second-decrease"))
-//     {
-//         sec--;
-//         sec=detail2(sec);
-//         view_second.textContent = detail(sec);
-//     }
-// }
