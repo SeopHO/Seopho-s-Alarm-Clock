5a829fa50de2992965ffa56bec5d6e5c871a27ab
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-01-20 21:05:54 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const start_btn = document.querySelector(".start-btn");
 
 let time_pos = null;
 
+let clickCheck = false;
+let keydonwCheck = false; 
+
 let hour_value=0;
 let min_value=0;
 let sec_value=0;
@@ -20,6 +23,9 @@ views.forEach(function(view)
 {
     view.addEventListener("click",function(event)
     {
+        clickCheck = true;
+        // console.log(clickCheck);
+        
         const style = event.currentTarget.classList;
         console.log(style);
         if(style.contains("view-hour"))
@@ -46,32 +52,66 @@ window.addEventListener("keydown",function(event)
         case 38:
             console.log('위');
             increaseHandler();
+            keydonwCheck=true;
             break;
         case 40:
             console.log('아래');
             decreaseHandler();
+            keydonwCheck=true;
             break;
     }
 });
 
+start_btn.addEventListener("click", function () 
+{
+    if (clickCheck === true && keydonwCheck == true )
+        if(hour_value != 0 || min_value != 0 || sec_value != 0)
+        {
+            let timer = setInterval(function () {
+                sec_value--;
+                if(sec_value < 0)
+                {
+                    min_value--;
+                    sec_value = 59;
+                }
+                if(min_value<0)
+                {
+                    hour_value--;
+                    min_value = 59;
+                }
+                view_second.textContent = detail(sec_value);
+                view_minute.textContent = detail(min_value);
+                view_hour.textContent=detail(hour_value);
+                if (hour_value === 0 && min_value === 0 && sec_value === 0) {
+                    clearInterval(timer);
+                }
+            }, 1000);
+        }
+});
+    
 function increaseHandler()
 {
     switch(time_pos)
     {
         case _HOUR:
             hour_value++;
-            view_hour.textContent = hour_value;
+            if (hour_value > 23)
+                hour_value = 0;
+            view_hour.textContent = detail(hour_value);
             break;
         case _MIN:
             min_value++;
-            view_minute.textContent = min_value;
+            if(min_value>59)
+                min_value=0;
+            view_minute.textContent = detail(min_value);
             break;
         case _SEC:
             sec_value++;
-            view_second.textContent = sec_value;
+            if(sec_value>59)
+                sec_value=0;
+            view_second.textContent = detail(sec_value);
             break;
     }
-    
 }
 
 function decreaseHandler()
@@ -80,17 +120,37 @@ function decreaseHandler()
     {
         case _HOUR:
             hour_value--;
-            view_hour.textContent = hour_value;
+            if(hour_value<0)
+                hour_value=23;
+            view_hour.textContent = detail(hour_value);
             break;
         case _MIN:
             min_value--;
-            view_minute.textContent = min_value;
+            if(min_value<0)
+                min_value=59;
+            view_minute.textContent = detail(min_value);
             break;
         case _SEC:
             sec_value--;
-            view_second.textContent = sec_value;
+            if(sec_value<0)
+                sec_value=59;
+            view_second.textContent = detail(sec_value);
             break;
     }
 }
+
+function detail(value)
+{
+    if(value<10)
+    {
+        return `0${value}`;
+    }
+    else
+    {
+        return value;
+    }
+}
+
+
 
 
